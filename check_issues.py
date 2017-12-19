bb01fb49f53717ac5834650d0e427031580a05da
Gloria Son <gloria.son@canada.ca>
2017-12-19 16:56:54 +0000

    added status history query and html formatting into email

--- check_issues.py
+++ check_issues.py
@@ -10,8 +10,7 @@ For hosts, there are only 3 codes: up, down or unreachable
 Host check URL:
 https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/3/en/hostchecks.html
 
-
-:author: Gloria Son
+:author: Gloria Son December 2017
 '''
 
 import argparse
@@ -21,6 +20,7 @@ import sys
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from datetime import datetime, timedelta
 import requests
 
 RECORDCOUNT_KEY = "recordcount"
@@ -40,28 +40,127 @@ CURRENT_STATE_SERVICE = {
     "3" : "UNKNOWN"
 }
 
-def send_email(html, recipients):
+#state history constants
+STATE_HISTORY_URL = "objects/statehistory"
+SERVICE_DESCRIPTION_KEY = "service_description"
+STATE_TIME_KEY = "state_time"
+OUTPUT_KEY = "output"
+CURRENT_CHECK_KEY = "current_check_attempt"
+MAX_CHECK_KEY = "max_check_attempts"
+STATE_KEY = "state"
+
+def send_email(html, recipients, detail):
     '''
     Send an email of the html Nagios summary
 
     :param html: html format of summary
     :param recipients: list of emails to send email to
+    :param detail: html format of detail tables
     '''
 
-    msg = MIMEMultipart('alternative')
+    msg = MIMEMultipart()
     msg['Subject'] = "Nagios XI Alert"
     msg['To'] = ",".join(recipients)
 
-    html_body = MIMEText(html, 'html')
+    message = "<html> " + html + "<br /> <p>Details</p>"+ "<br />".join(detail) + "</html>"
+    html_body = MIMEText(message, 'html')
     msg.attach(html_body)
 
     smtp = smtplib.SMTP('localhost')
     smtp.sendmail('localhost', recipients, msg.as_string())
     smtp.quit()
 
+def create_details_html(detail_summary, historylist):
+    '''
+    STATE HISTORY
+    Format html tables for the state history query results
+
+    :param detail_summary: long string that contains all the detail tables
+    :param historylist: an entry from the api state history response
+    '''
+    table = ""
+    host = historylist[0][HOST_NAME_KEY]
+    # append row to a table of an entry
+    for row in historylist:
+        table += '''
+        <tr>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}/{}</td>
+            <td>{}</td>
+        </tr>
+        '''.format(
+            row[SERVICE_DESCRIPTION_KEY],
+            row[STATE_KEY],
+            row[STATE_TIME_KEY],
+            row[CURRENT_CHECK_KEY],
+            row[MAX_CHECK_KEY],
+            row[OUTPUT_KEY])
+
+    #add the table to the entire detail html string
+    detail_summary.append('''
+    <table border="1">
+        <caption>{}</caption>
+        <tr>
+            <th>Service</th>
+            <th>Status</th>
+            <th>Time</th>
+            <th>Checks</th>
+            <th>Description</th>
+        </tr>
+        <tbody>{}</tbody>
+    </table>
+    '''.format(host, table))
+
+def detail_query(base_url, api_key, details, detail_summary):
+    '''
+    STATE HISTORY
+    Create the query string and get the results from the nagios api
+
+    :param base_url: nagios api website
+    :param api_key: api key to access
+    :param details: list of hosts/services histories to be checked
+    :param detail_summary: string that will hold the html format of
+        finalized the detail summary
+    '''
+    #get the last hour date string
+    last_hour = datetime.utcnow() - timedelta(hours=1)
+    last_hour_str = last_hour.strftime("%s")
+
+    #create the base of the query string
+    base_url = "{}{}?apikey={}".format(
+        base_url if base_url.endswith("/") else base_url + "/",
+        STATE_HISTORY_URL,
+        api_key)
+
+    for entry in details:
+        params = []
+        params.append("{}={}".format("starttime", last_hour_str))
+        params.append("{}=lk:{}".format(HOST_NAME_KEY, entry["host"]))
+        params.append("{}=lk:{}".format(SERVICE_DESCRIPTION_KEY, entry["service"]))
+        url = base_url + "&" + "&".join(params)
+
+        try:
+            #use requests to get the JSON response
+            response = requests.get(url)
+            response = json.loads(response.text)
+            historylist = response.values()[0]
+
+            #if there is content in the response, an html will be created
+            if int(historylist[RECORDCOUNT_KEY]) > 0:
+                del historylist[RECORDCOUNT_KEY]
+                historylist = historylist.values()[0]
+                if not isinstance(historylist, list):
+                    historylist = [historylist]
+                create_details_html(detail_summary, historylist)
+        except requests.exceptions.ConnectionError:
+            logging.error("Cannot query %s", url)
+
 def html_format(hosts, services):
     '''
-    Creates the html format of the summary
+    STATUS
+    Creates the html format of the status summary
 
     :param hosts: list of hosts with issues
     :param services: list of services with issues
@@ -95,7 +194,6 @@ def html_format(hosts, services):
             </tr>'''.format(link, host_name, name, status, status_time)
 
     html = '''
-    <html>
         <table border="1">
             <caption>Host Issues</caption>
             <tr>
@@ -116,13 +214,13 @@ def html_format(hosts, services):
             </tr>
             <tbody> {} </tbody>
         </table>
-    </html>
     '''.format(host_html, service_html)
     return html
 
 def write_soh_summary(filename, hosts, services):
     '''
-    Writes results of hosts/services with issues into a file
+    STATUS
+    Writes results of hosts/services with issues into a file/stdout
 
     :param filename: file to write list of hosts/services to
     :param hosts: list of hosts with issues
@@ -147,9 +245,9 @@ def write_soh_summary(filename, hosts, services):
     if file_opened:
         fptr.close()
 
-
-def get_query(url, host_list, service_list, base_redirect):
+def get_query(url, host_list, service_list, base_redirect, detail_check):
     '''
+    STATUS
     Retrieves data from nagios api and adds entries to appropriate host/service list
 
     :param url: URL for request to nagios api
@@ -171,7 +269,7 @@ def get_query(url, host_list, service_list, base_redirect):
             statuslist = [statuslist]
 
         for entry in statuslist:
-            # If the key HOST_NAME exists, its a service
+            # If the key HOST_NAME exists, it's a service
             # We convert the CURRENT_STATE to readable format
             if HOST_NAME_KEY in entry:
                 redirect_link = base_redirect + entry[HOST_NAME_KEY]
@@ -181,6 +279,7 @@ def get_query(url, host_list, service_list, base_redirect):
                 service_list.append("{}: {} [{}] '{}' {}".format(
                     entry[HOST_NAME_KEY], entry[NAME_KEY], state,
                     entry[STATUS_UPDATE_TIME_KEY], redirect_link))
+                detail_check.append({"host": entry[HOST_NAME_KEY], "service": entry[NAME_KEY]})
             else:
                 redirect_link = base_redirect + entry[NAME_KEY]
                 state = CURRENT_STATE_HOST.get(
@@ -188,9 +287,11 @@ def get_query(url, host_list, service_list, base_redirect):
                     "Unknown state value %s" % entry[CURRENT_STATE_KEY])
                 host_list.append("{} [{}] '{}' {}".format(
                     entry[NAME_KEY], state, entry[STATUS_UPDATE_TIME_KEY], redirect_link))
+                detail_check.append({"host": entry[NAME_KEY], "service": ""})
 
 def create_query(config):
     '''
+    STATUS
     Creates query string and gets response request from nagios api
 
     :param config: configuration
@@ -204,6 +305,7 @@ def create_query(config):
     base_redirect = config["redirect"]
     host_list = []
     service_list = []
+    detail_check = []
     del config["nagiosapi"], config["apikey"], config["redirect"]
 
     # parse through the service and host objects in the configuration file
@@ -235,11 +337,11 @@ def create_query(config):
             logging.info("Querying: " + url)
 
             try:
-                get_query(url, host_list, service_list, base_redirect)
+                get_query(url, host_list, service_list, base_redirect, detail_check)
             except requests.exceptions.ConnectionError:
                 logging.error("Cannot query %s", url)
 
-    return host_list, service_list
+    return host_list, service_list, detail_check
 
 def main():
     '''main'''
@@ -270,7 +372,13 @@ def main():
         sys.exit(1)
 
     logging.info("Obtained JSON data")
-    hosts, services = create_query(config)
+
+    api = config["nagiosapi"]
+    api_key = config["apikey"]
+    hosts, services, details = create_query(config)
+
+    detail_summary = []
+    detail_query(api, api_key, details, detail_summary)
     html = ""
 
     write_soh_summary(args.output, hosts, services)
@@ -278,7 +386,7 @@ def main():
         html = html_format(hosts, services)
         logging.info(html)
     if args.email:
-        send_email(html, args.email)
+        send_email(html, args.email, detail_summary)
 
 if __name__ == "__main__":
     main()
